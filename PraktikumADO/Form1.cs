edf10d303b3f0a7bbb800adfd40b88b4910ac06d
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-03-12 23:51:01 +0700

    tombol connect db

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,3 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+
+namespace PraktikumADO
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Koneksi()
+        {
+            conn = new SqlConnection("Data Source=FADLANNASRIZAL\\FADLAN;Initial Catalog=DBAkademikADO;Integrated Security=True");
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+    }
+}
+
